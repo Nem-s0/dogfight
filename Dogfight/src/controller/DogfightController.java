0e63a503ec675a75ae03c70002850bfe791f9cb0
Nem's <Nem's@NEM-S>
2022-10-12 16:41:51 +0100

    phase2

--- Dogfight/src/controller/DogfightController.java
+++ Dogfight/src/controller/DogfightController.java
@@ -2,15 +2,17 @@ package controller;
 
 public class DogfightController {
 private static int TIME_SLEPP =30;
-
+	public DogfightController (IDOgfightModel dogfightMOdel) {
+}
 	public void orderPerform (UserOrder userOrder) {
 	}
 	public void play(){
 	}
-
+	public void setViewSystem (IViewSystem viewSystem) {
+	}
 	private void lauchMissile (int player) {
 	}
-	private void gameLoop () {
-		
+	private void gameLoop () {	
 	}
 }
+
